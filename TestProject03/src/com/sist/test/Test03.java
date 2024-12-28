e0b26638b9d7110abfc4f9b5ddc37e985740b5ad
Moon-Jongho <1001moon@hanmail.net>
2024-12-28 19:06:21 +0900

    20241228 19:06

--- TestProject03/src/com/sist/test/Test03.java
+++ TestProject03/src/com/sist/test/Test03.java
@@ -5,6 +5,7 @@ public class Test03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1001moon에서 Test03 프로젝트를 생성");
+		System.out.println("1001moon에서 Test03 프로젝트를 생성");
 
 	}
 
