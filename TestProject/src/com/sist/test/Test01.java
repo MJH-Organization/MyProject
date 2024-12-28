e9d5dc1e669d52fd7947b831225c50e544bc2632
Moon-Jongho <1001moon@hanmail.net>
2024-12-28 18:13:30 +0900

    1234

---
+++ TestProject/src/com/sist/test/Test01.java
@@ -0,0 +1,11 @@
+package com.sist.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test01");
+		System.out.println("Test02");
+	}
+
+}
