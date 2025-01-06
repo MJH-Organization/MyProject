8f16b826263ca4768407b7a5a71435174083ee1e
sist_108 <sist_108@sist108>
2025-01-06 16:32:54 +0900

    20250106 16:32

---
+++ MovieProject/src/main/java/com/sist/Movie.java
@@ -0,0 +1,15 @@
+// 사용자 정의 데이터형
+package com.sist;
+
+public class Movie {
+	public String rank;
+	public String title,genre,poster,actor,regdate,grade,director;
+
+//	int no; 
+//	String title;
+//	String	genre;
+//	String	actor;
+//	String	regdate;
+//	String	grade;
+//	String	director;
+}
---
+++ MovieProject/src/main/java/com/sist/MovieMain.java
@@ -0,0 +1,20 @@
+package com.sist;
+
+import javax.swing.*;
+
+public class MovieMain  extends JFrame{
+	
+	MovieMain()
+	{
+		this.setTitle("창열기 연습");
+		this.setSize(400, 500);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MovieMain movie=new MovieMain();
+
+	}
+
+}
